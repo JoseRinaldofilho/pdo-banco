68d56fc2096af7e1a16939acd529819ccdd07dd8
JoseRinaldofilho <jose.vasconcelos@basis.com.br>
2021-10-29 14:49:59 -0300

    conexao.php feita

--- Poo_Digital_Innovation/Banco_dados_PDO/conexao.php
+++ Poo_Digital_Innovation/Banco_dados_PDO/conexao.php
@@ -5,8 +5,11 @@
 $pdo = null;
 
 try {
-
-
+                        //banco               esquema             usuario        senha
+    $pdo = new PDO('mysql:dbname=crud_pdo','root','');
+    echo "Logado com SUceSSo!!";
 }catch (PDOException $e){
     echo "Erro no banco".$e->getMessage();
+}catch (Exception $e){
+    echo "Erro -> ".$e->getMessage();
 }
--- sistemaLogin/conectar.php
+++ sistemaLogin/conectar.php
@@ -7,6 +7,7 @@ try {
 catch (PDOException $e) {
     echo "Erro com Banco de dados : " . $e->getMessage;
 
+
 } catch (Exception $e) {
     echo "ERROR : " . $e->getMessage;
 }
